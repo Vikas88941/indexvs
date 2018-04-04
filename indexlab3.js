df1348f922bbd825ce7f150b7d6ee6c8d0523e4e
Vikas88941 <35552306+Vikas88941@users.noreply.github.com>
2018-04-04 10:02:10 +0530

    Add files via upload
    11501172

---
+++ indexlab3.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const http = require('http');
+const hostname = 'localhost';
+const port =3000;
+const app =express();
+const server=http.createServer((req,res)=>{
+	
+console.log('Request for'+ req.url+'by method'+req.method);
+if(req.method=='GET')
+	
+	{
+		var fileUrl;
+		if(req.url == '/')
+	    fileUrl= '/vikas.html';
+	}
+	else{
+		fileUrl=req.url;
+     var filePath=path.resolve('./public'+fileUrl);
+	const fileExt=path.extname(filePath);	
+	}
+	if(fileExt =='.html')
+		{
+		fs.exists(filePath,(exists) => {
+		if(exists);
+{
+			res.statusCode=404;
+			res.setHeader('Content-Type','text/html');
+			res.end('<html><body><h1>Error:404'+fileUrl+'not found</h1></body></html>');
+			return;
+			}		
+	res.statusCode=200;
+			res.setHeader('Content-Type','text/html');
+			res.end('<html><body><h1>Success:</h1></body></html>');
+				
+			fs.createReadStream(filePath).pipe(res);
+			})
+	}
+	else
+	{
+		res.statusCode=404;
+		res.setHeader('Content-Type','text/html');
+		
+		res.end('<html><body><h1>Error:404'+fileUrl+'not a html file</h1></body></html>');
+	}
+	
+})
+
+	server.listen(port,hostname, () =>{
+	console.log('server running at http://${hostname}:${port}/');	
+});
